f7835d1d975885966eefad6d22a27370bb78ada2
Ashma <ashma@example.com>
2010-12-01 22:22:29 +0000

    geting photosize
    git-svn-id: svn://okcodev/framework/trunk/code@114 a63f4af5-f9c0-4d22-9a79-dc730627376b

--- functions.php
+++ functions.php
@@ -93,6 +93,31 @@ function htmlize () {
 	}
 	return $str;
 }
+function photo_size($photo, $max_width, $max_height, $return_value = "w")
+{
+	$dimensions = @getimagesize($photo);
+	$width = $dimensions[0];
+	$height = $dimensions[1];
+  
+	if($width > $max_width || $height > $max_height)
+  { 
+	  if($width > $max_width)
+	{
+		$height = $height*$max_width/$width;
+		$width = $max_width;
+	  }
+	
+	  if($height > $max_height)
+	{
+		$width = $width*$max_height/$height;
+		$height = $max_height;
+	  }
+	}
+  
+	if($return_value == "w") { $image_dimension = $width; } else { $image_dimension = $height; }
+  
+	return round($image_dimension, 2);
+ }
 if (!function_exists('money_format')) {
 	function money_format ($format, $number) {
 		$regex  = '/%((?:[\^!\-]|\+|\(|\=.)*)([0-9]+)?'.

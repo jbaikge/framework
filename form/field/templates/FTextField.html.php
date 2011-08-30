77396473c3a8be12377ef806b6a38524c1395282
mark <ldapmonkey@gmail.com>
2011-08-30 17:22:06 +0000

    Added 'size' attribute to the FTextField template.
    git-svn-id: svn://okcodev/framework/trunk/code@334 a63f4af5-f9c0-4d22-9a79-dc730627376b

--- form/field/templates/FTextField.html.php
+++ form/field/templates/FTextField.html.php
@@ -1,2 +1,8 @@
 <?php echo FTemplate::fetchCached(dirname(__FILE__) . '/FLabelField.html.php', array('form' => &$form, 'field' => &$field)); ?>
-<input type="text" value="<?php e($field->getValue()); ?>" name="<?php echo $field->getName(); ?>" id="<?php echo $field->getId(); ?>">
+<?php
+$attributes = '';
+if ($field->get('size')) { 
+	$attributes .= ' size="' . $field->get('size') . '"';
+}
+?>
+<input type="text" value="<?php e($field->getValue()); ?>" name="<?php echo $field->getName(); ?>" id="<?php echo $field->getId(); ?>"<?php echo $attributes; ?>>
